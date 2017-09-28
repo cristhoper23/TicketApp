9a1d1277dac9f92a688ecc25e257627c49d304be
RubyMan <RubyMan@gmail.com>
2017-09-27 20:35:29 -0500

    3° commit

--- app/src/main/java/com/cristhoper/ticket_app/activities/UserActivity.java
+++ app/src/main/java/com/cristhoper/ticket_app/activities/UserActivity.java
@@ -5,9 +5,13 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cristhoper.ticket_app.R;
@@ -21,19 +25,22 @@ public class UserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        // Setear Toolbar como action bar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         // Set DrawerLayout
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        // Set drawer toggle icon
-        final ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+        //Botón action bar drawer con efecto material
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, android.R.string.ok, android.R.string.cancel);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         // Set NavigationItemSelectedListener
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
                 // Do action by menu item id
@@ -72,40 +79,23 @@ public class UserActivity extends AppCompatActivity {
             }
         });
 
+        // Change navigation header information
+
         if (this.getIntent() != null && this.getIntent().getExtras() != null) {
             String rol = this.getIntent().getExtras().getString("rol");
 
             switch (rol){
                 case "Cliente" :
-                    navigationView.getMenu().findItem(R.id.nav_registrar).setVisible(true);
-                    navigationView.getMenu().findItem(R.id.nav_consultar).setVisible(true);
-                    navigationView.getMenu().findItem(R.id.nav_asignar).setVisible(false);
-                    navigationView.getMenu().findItem(R.id.nav_reporte).setVisible(false);
-                    navigationView.getMenu().findItem(R.id.nav_atender).setVisible(false);
-                    navigationView.getMenu().findItem(R.id.nav_settings).setVisible(true);
-                    navigationView.getMenu().findItem(R.id.nav_logout).setVisible(true);
+                    rolInterface(navigationView, rol, "Roy Anderson", "roy.anderson@gmail.com", true, true, false, false, false);
                     break;
                 case "Supervisor" :
-                    navigationView.getMenu().findItem(R.id.nav_registrar).setVisible(false);
-                    navigationView.getMenu().findItem(R.id.nav_consultar).setVisible(true);
-                    navigationView.getMenu().findItem(R.id.nav_asignar).setVisible(true);
-                    navigationView.getMenu().findItem(R.id.nav_reporte).setVisible(true);
-                    navigationView.getMenu().findItem(R.id.nav_atender).setVisible(false);
-                    navigationView.getMenu().findItem(R.id.nav_settings).setVisible(true);
-                    navigationView.getMenu().findItem(R.id.nav_logout).setVisible(true);
+                    rolInterface(navigationView, rol, "Stan Leabreu", "stan.leabreu@gmail.com", false, true, true, true, false);
                     break;
                 case "Técnico" :
-                    navigationView.getMenu().findItem(R.id.nav_registrar).setVisible(false);
-                    navigationView.getMenu().findItem(R.id.nav_consultar).setVisible(true);
-                    navigationView.getMenu().findItem(R.id.nav_asignar).setVisible(false);
-                    navigationView.getMenu().findItem(R.id.nav_reporte).setVisible(false);
-                    navigationView.getMenu().findItem(R.id.nav_atender).setVisible(true);
-                    navigationView.getMenu().findItem(R.id.nav_settings).setVisible(true);
-                    navigationView.getMenu().findItem(R.id.nav_logout).setVisible(true);
+                    rolInterface(navigationView, rol, "Johnny Cage", "johnny.cage@gmail.com", false, true, false, false, true);
                     break;
             }
         }
-
     }
 
     @Override
@@ -121,4 +111,20 @@ public class UserActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void rolInterface(NavigationView navView, String rol, String nombre, String correo, boolean a, boolean b, boolean c, boolean d, boolean e){
+        ImageView photoImage = (ImageView) navView.getHeaderView(0).findViewById(R.id.menu_photo);
+        photoImage.setBackgroundResource(R.drawable.ic_profile);
+
+        TextView fullnameText = (TextView) navView.getHeaderView(0).findViewById(R.id.menu_fullname);
+        fullnameText.setText(rol + ": " + nombre);
+
+        TextView emailText = (TextView) navView.getHeaderView(0).findViewById(R.id.menu_email);
+        emailText.setText(correo);
+
+        navView.getMenu().findItem(R.id.nav_registrar).setVisible(a);
+        navView.getMenu().findItem(R.id.nav_consultar).setVisible(b);
+        navView.getMenu().findItem(R.id.nav_asignar).setVisible(c);
+        navView.getMenu().findItem(R.id.nav_reporte).setVisible(d);
+        navView.getMenu().findItem(R.id.nav_atender).setVisible(e);
+    }
 }
